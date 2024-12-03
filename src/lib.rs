5b24c70ed6b73a614c7e820a28b2cb4625949a2b
pn00@dearch <prateek.n2000@ymail.com>
2024-12-04 01:13:56 +0530

    day1 complete

---
+++ src/lib.rs
@@ -0,0 +1,80 @@
+mod solution;
+
+pub fn init() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args  = std::env::args();
+    args.next();
+
+    let day = args.next().expect(r#"Which day to solve? Run 'cargo run -- ${day number} ${part number}'."#).parse::<u8>()?;
+    let part = Part::parse(args.next().expect(r#"Which part to solve? Run 'cargo run -- ${day number} ${part number}'"#))?;
+    let path = match args.next() {
+        Some(arg) if arg == "test" => format!("input/day{day}-part{part}_test.txt"),
+        Some(arg) => panic!("Invalid argument {} passed. Run 'cargo run -- 1 test' to run test code", arg),
+        None => format!("input/day{day}-part{part}.txt"),
+    };
+
+    let input = std::fs::read_to_string(path)?;
+    let sol = get_solution(day);
+
+    let instant = std::time::Instant::now();
+    let ans = match part {
+        Part::One => sol.part_1(input)?,
+        Part::Two => sol.part_2(input)?,
+    };
+    let elapsed = instant.elapsed();
+
+    println!("ans: {ans}. Time taken {elapsed:?}");
+    Ok(())
+}
+
+fn get_solution(day: u8) -> impl Solution {
+    match day {
+        1 => solution::Day1,
+        _ => unimplemented!(),
+    }
+}
+
+trait Solution {
+    fn part_1(&self, input: String) -> Result<usize, Error>;
+    fn part_2(&self, input: String) -> Result<usize, Error>;
+}
+
+#[derive(Debug)]
+enum Error {
+    InvalidInput(String),
+    InvalidArgument,
+}
+
+impl std::error::Error for Error {}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::InvalidArgument => write!(f, "Invalid argument passed."),
+            Self::InvalidInput(reason) => write!(f, "Input invalid. {reason}"),
+        }
+    }
+}
+
+enum Part {
+    One,
+    Two,
+}
+
+impl Part {
+    fn parse(input: String) -> Result<Self, Error> {
+        match input.as_str() {
+            "1" | "One" | "one" | "ONE" => Ok(Self::One),
+            "2" | "Two" | "two" | "TWO" => Ok(Self::Two),
+             _ =>  Err(Error::InvalidArgument),
+        }
+    }
+}
+
+impl std::fmt::Display for Part {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{0}", match self {
+            Self::One => "1",
+            Self::Two => "2",
+        })
+    }
+}
---
+++ src/solution/mod.rs
@@ -0,0 +1,39 @@
+use super::{Solution, Error};
+use std::collections::HashMap;
+
+pub struct Day1;
+
+impl Solution for Day1 {
+    fn part_1(&self, input: String) -> Result<usize, Error> {
+        let mut re: (Vec<usize>, Vec<usize>) = input.lines().map(|l| {
+            let mut s = l.split_whitespace();
+            let parse = |s: Option<&str>| s.expect("invalid input").parse::<usize>().expect("unable to parse input");
+            (parse(s.next()), parse(s.next()))
+        }).unzip();
+
+        re.0.sort();
+        re.1.sort();
+
+        let ans = re.0.into_iter().zip(re.1).map(|(v1, v2)| v1.abs_diff(v2)).sum::<usize>();
+
+        Ok(ans)
+    }
+
+    fn part_2(&self, input: String) -> Result<usize, Error> {
+        let re: (Vec<usize>, Vec<usize>) = input.lines().map(|l| {
+            let mut s = l.split_whitespace();
+            let parse = |s: Option<&str>| s.expect("invalid input").parse::<usize>().expect("unable to parse input");
+            (parse(s.next()), parse(s.next()))
+        }).unzip();
+
+        let data = re.1.into_iter().fold(HashMap::<usize, (usize, usize)>::new(), |mut acc, val| {
+            acc.entry(val).and_modify(|v| v.1 += 1).or_insert((0, 1));
+            acc
+        });
+
+        Ok(re.0.into_iter().fold(data, |mut acc, val| {
+            acc.entry(val).and_modify(|v| v.0 += 1);
+            acc
+        }).into_iter().map(|(k, (v1, v2))| k * v1 * v2).sum::<usize>())
+    }
+}
